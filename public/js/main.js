02df747f3079a7e4ea640a8ea972621c102881bf
timurlog <timur.logie@gmail.com>
2023-11-16 10:27:28 +0100

    modified main.js Computer Fonction init

---
+++ public/js/main.js
@@ -0,0 +1,15 @@
+console.log("Hello World");
+
+// Computer variable
+let answer = null;
+let answers = [ "rock",
+"paper",
+"scissors"];
+
+let index = Math.floor(Math.random() * answers.length);
+// console.log(answers[index]);
+
+// ComputerChoice Fonction
+function getComputerChoice() {
+    
+}
